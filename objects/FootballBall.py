2dca42220aa1cb89ff2a092d5fb0c127e47e54a6
Huynh Phuc Nguyen Nguyen <hngu0112@student.monash.edu>
2025-06-16 12:15:09 +1000

    adding team and displayer score for each team. move the field to middle of the screen with some extra space outside

--- objects/FootballBall.py
+++ objects/FootballBall.py
@@ -2,9 +2,9 @@ import math
 import pygame
 
 class FootballBall:
-    FRICTION = 0.9999 # friction slows down the ball each update
+    FRICTION = 0.995 # friction slows down the ball each update
 
-    def __init__(self, x, y, radius=10, mass=1, colour=(255, 255, 255)):
+    def __init__(self, x, y, radius=10, mass=100, colour=(255, 255, 255)):
         self.x = x
         self.y = y
         self.radius = radius
@@ -14,15 +14,34 @@ class FootballBall:
         self.vel_x = 0
         self.vel_y = 0
 
-    def update(self, dt):
-        # Move ball by current velocity
+    def update(self, world, dt):
+        self.update_position(dt)
+        self.check_bouncing(world.field)
+        self.update_speed(dt)
+    def update_position(self,dt):
         self.x += self.vel_x * dt
         self.y += self.vel_y * dt
+    def check_bouncing(self,field):
+        left = field.offset
+        right = field.offset + field.length
+        top = field.offset
+        bottom = field.offset + field.width
 
-        # Apply friction to slow ball down
+        if self.x - self.radius < left:
+            self.x = left + self.radius
+            self.vel_x *= -1
+        elif self.x + self.radius > right:
+            self.x = right - self.radius
+            self.vel_x *= -1
+
+        if self.y - self.radius < top:
+            self.y = top + self.radius
+            self.vel_y *= -1
+        elif self.y + self.radius > bottom:
+            self.y = bottom - self.radius
+            self.vel_y *= -1
+    def update_speed(self, dt):
         self.vel_x *= self.FRICTION
         self.vel_y *= self.FRICTION
-
-
     def draw(self, surface):
         pygame.draw.circle(surface, self.colour, (int(self.x), int(self.y)), self.radius)
