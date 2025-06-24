ee66e5e97aa913d748e47ee0a87e11d2472a124f
Huynh Phuc Nguyen Nguyen <hngu0112@student.monash.edu>
2025-06-24 18:25:00 +1000

    fix the front size if the resolution is change

--- GameView.py
+++ GameView.py
@@ -9,8 +9,8 @@ class GameView:
         self.screen.fill(self.world.field.colour)  # Grass background
         counter = 0
         for team in self.world.teams:
-            team.draw(self.screen,(counter,0))
-            counter+= 300
+            team.draw(self.screen,(counter,0),self.world.SCALE)
+            counter+= 300*self.world.SCALE
         # Draw all players
         for object in self.world.objects:
             object.draw(self.screen)
--- objects/FootballTeam.py
+++ objects/FootballTeam.py
@@ -6,8 +6,8 @@ class FootballTeam:
         self.score = 0
         self.is_on_left_side = is_on_left_side
 
-    def draw(self, surface, position):
-        font = pygame.font.SysFont(None, 40)
+    def draw(self, surface, position, window_scale):
+        font = pygame.font.SysFont(None, int(40*window_scale))
         text = f"{self.name}: {self.score}"
         rendered_text = font.render(text, True, self.colour)
         surface.blit(rendered_text, position)
